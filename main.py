93b1bd9cc951bbbbbb76660410ac42e65f484702
abaker78 <46098201+abaker78@users.noreply.github.com>
2021-09-15 22:11:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import discord
 from discord.ext import commands
 import os
 
+
 bot = commands.Bot(command_prefix='%', case_insensitive=True)
 
 my_secret = os.environ['REDBOT']
 client = discord.Client()
-
 extensions = ['cogs.Administrator']
+
 if __name__ == '__main__':
     for ext in extensions:
         bot.load_extension(ext)
+        
 bot.run(my_secret)
