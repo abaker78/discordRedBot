3d2507439385d16993c754b4160dbb6de2416773
abaker78 <46098201+abaker78@users.noreply.github.com>
2021-09-15 22:10:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot = commands.Bot(command_prefix='%', case_insensitive=True)
 my_secret = os.environ['REDBOT']
 client = discord.Client()
 
-extensions = ['cogs.Greetings', 'cogs.Administrator']
+extensions = ['cogs.Administrator']
 if __name__ == '__main__':
     for ext in extensions:
         bot.load_extension(ext)
