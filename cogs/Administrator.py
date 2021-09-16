d2a96dce287afda8aaa6042434d24102941da4a5
abaker78 <46098201+abaker78@users.noreply.github.com>
2021-09-15 23:34:17 -0500

    Update Administrator.py

--- cogs/Administrator.py
+++ cogs/Administrator.py
@@ -6,14 +6,22 @@ class Administrator(commands.Cog):
         self.bot = bot
 
     @commands.command(name='dChannel', aliases=['remove'])
-    async def remove_channel(self, ctx, channel: d.TextChannel):
-        mbed = d.Embed(
-            title = 'Success',
-            description = f'Channel: "{channel}" has been deleted',
-        )
+    async def remove_channel(self, ctx, *channel: d.TextChannel):
+       
         if ctx.author.guild_permissions.manage_channels:
-            await ctx.send(embed=mbed)
-            await channel.delete()
+        
+            for i in channel:
+                await i.delete()
+                mbed = d.Embed(
+                    title = 'Success',
+                    description = f'Channel: {i} has been deleted',
+                    color=0xFF5733
+                )
+                mbed.set_author(
+                    name=ctx.author.display_name,
+                    icon_url=ctx.author.avatar_url
+                )
+                await ctx.send(embed=mbed)
 
 def setup(bot):
     bot.add_cog(Administrator(bot))
